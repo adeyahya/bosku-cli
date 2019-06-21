921bb4ccc95acdca8549ff3bee90f7751faebeed
Ade Yahya <adeyahyaprasetyo@gmail.com>
2019-06-21 23:49:43 +0700

    create npm command

---
+++ src/actions/npm.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const {getMonorepoDir} = require("../helpers/utilities");
+const {yaml} = require('../helpers/parser');
+const spawn = require('../helpers/spawn');
+
+module.exports = (projectName, otherProject) => {
+  const projects = [projectName, ...otherProject].map(item => {
+    const setup = item.split(':');
+    return {
+      name: setup[0],
+      script: setup[1],
+    };
+  });
+  const monorepoDir = getMonorepoDir();
+  if (monorepoDir) {
+    projects.forEach(project => {
+      const cwd = path.join(monorepoDir, project.name);
+      if (fs.existsSync(cwd)) {
+        spawn(project.name, 'yarn', [project.script || 'dev'], {cwd});
+      } else {
+        throw Error(`project ${project.name} not found!`)
+      }
+    })
+  } else {
+    // missing monorepo
+  }
+}
--- src/cli.js
+++ src/cli.js
@@ -4,9 +4,11 @@ const info = require("../package.json");
 
 // actions
 const develop = require('./actions/develop');
+const npm = require('./actions/npm');
 
 program
   .version(info.version)
+  .command("npm <projectName> [otherProject...]").action(npm)
   .command("develop <projectName>").action(develop)
 
 program.parse(process.argv);
